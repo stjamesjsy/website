a3bf7217739bdbb935f0d8285dc60c6744c84554
Luke <luke.wyatt21@gmail.com>
2026-06-01 22:10:46 +0100

    Add google analytics back

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -39,6 +39,18 @@ export default function App({ Component, pageProps }: AppPropsWithConfig) {
 
     return (
         <ChakraProvider value={defaultSystem}>
+            <div dangerouslySetInnerHTML={{ __html: '<!-- Website created by Luke (luke@glitch.je) -->' }} />
+
+            <meta charSet="utf-8" />
+
+            <meta name="viewport" content="width=device-width,initial-scale=1" />
+            <meta name="author" content="Luke <luke@glitch.je>" />
+            <meta name="description" content="St James Jersey" />
+            <meta name="keywords" content="nightclub, karaoke, bingo, food, sing, jersey, channel islands" />
+            <meta name="robots" content="index,follow" />
+
+            <title>St James</title>
+
             <link rel="preconnect" href="https://fonts.googleapis.com" />
             <link rel="preconnect" href="https://fonts.gstatic.com" />
             <link href="https://fonts.googleapis.com/css2?family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&family=Plus+Jakarta+Sans:ital,wght@0,200..800;1,200..800&display=swap" rel="stylesheet" />
--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,4 +1,5 @@
 import { Html, Head, Main, NextScript } from "next/document";
+import Script from "next/script";
 
 export default function Document() {
     return (
@@ -7,6 +8,19 @@ export default function Document() {
             <body>
                 <Main />
                 <NextScript />
+
+                <Script
+                    src="https://www.googletagmanager.com/gtag/js?id=G-WCZTFPN1W9"
+                    strategy="afterInteractive"
+                />
+                <Script id="gtag-init" strategy="afterInteractive">
+                    {`
+                        window.dataLayer = window.dataLayer || [];
+                        function gtag(){dataLayer.push(arguments);}
+                        gtag('js', new Date());
+                        gtag('config', 'G-WCZTFPN1W9');
+                    `}
+                </Script>
             </body>
         </Html>
     );
