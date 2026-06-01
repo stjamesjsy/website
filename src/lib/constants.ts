05139c4781162e632f8d1f47296491de2998a1b3
Luke <luke.wyatt21@gmail.com>
2026-06-01 22:08:54 +0100

    Update basket meals

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -12,12 +12,12 @@ export const WINE_MENU_URL = "/files/WineMenu-June2026.pdf";
 export const BASKET_MEALS = [
     { name: "Prawns Tempura", price: 9.50 },
     { name: "Fish & Chips", price: 9.50 },
-    { name: "Prawns in Filo", price: 9.50 },
     { name: "Whole Tail Breaded Scampi", price: 9.50 },
     { name: "Southern Fried Chicken Goujons", price: 9.50 },
     { name: "Gluten Free Chicken Goujons", price: 9.50 },
     { name: "Vegetarian Spring Rolls", price: 9.50 },
     { name: "Sweet Potato Falafel", price: 9.50 },
     { name: "Halloumi Fries", price: 9.50 },
-    { name: "Chips", price: 5 }
+    { name: "Chips", price: 5 },
+    { name: "Onion Rings", price: 5 }
 ];
